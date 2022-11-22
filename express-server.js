8e5177b3c0d6de15251c5454ff188091e4bb5a2d
alagu-p <alagappan.plr@gmail.com>
2022-11-22 06:52:22 +0530

    Day-70

--- express-server.js
+++ express-server.js
@@ -6,6 +6,26 @@ app.get("/",(req,res)=>{
     res.send("<h1>Welcome to the home page...</h1>");
 
 });
+app.get("/about",(req,res)=>{
+    res.send("<h1>Welcome to the about page...</h1>");
+
+});
+app.get("/mani",(req,res)=>{
+    res.send("<h1>Welcome to the mani page...</h1>");
+
+});
+
+app.get("/alagu",(req,res)=>{
+    res.send("<h1>Welcome to the alagu page...</h1>");
+
+});
+
+app.get("/sir",(req,res)=>{
+    res.send("<h1>Welcome to the sir page...</h1>");
+
+});
+
+
 app.listen("5000",()=>{
     console.log("server running on port 5000");
 });
